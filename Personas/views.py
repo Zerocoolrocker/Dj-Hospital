f3255c986c23904143148733dfabab9814f2b88a
root <zerocoolrocker@gmail.com>
2016-03-27 16:24:15 -0430

    Nuevas vistas de pacientes

--- Personas/views.py
+++ Personas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from django.core.urlresolvers import reverse_lazy
+from django.views.generic import DetailView
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
@@ -16,14 +17,18 @@ class RegistroPaciente(CreateView):
 	model = Paciente
 	template_name = "registro.html"
 	success_url = reverse_lazy("home")
-	fields = ["nombre", "identificacion", "apellido", "identificacion", "habitacion", "fecha_ingreso", "diagnostico"]
+
+
+class DetallesPaciente(DetailView):
+	model = Paciente
+	template_name = "detalles_paciente.html"
 
 
 class EditarPaciente(UpdateView):
 	model = Paciente
 	template_name = "registro.html"
 	success_url = reverse_lazy("home")
-	fields = ["nombre", "identificacion", "apellido", "identificacion", "habitacion", "fecha_ingreso", "diagnostico"]
+
 
 class Pacientes(ListView):
 
--- hospital/urls.py
+++ hospital/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from Personas.views import RegistroPaciente, Home, Pacientes
+from Personas.views import RegistroPaciente, EditarPaciente, Home, Pacientes, DetallesPaciente
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,4 +12,6 @@ urlpatterns = patterns('',
     # url(r'^pacientes/registro/', "Personas.views.registro_paciente"),
     url(r'^pacientes/$', Pacientes.as_view(), name="lista-pacientes"),
     url(r'^pacientes/registro/$', RegistroPaciente.as_view(), name="registro-paciente"),
+    url(r'^pacientes/editar/(?P<pk>[\d]+)/$', EditarPaciente.as_view(), name="registro-paciente"),
+    url(r'^pacientes/(?P<pk>[\d]+)/$', DetallesPaciente.as_view(), name="detalles-paciente"),
 )
